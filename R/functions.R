17933c4c4ef1baedaff316c4139663582bd89235
mdri00 <drieumathis@gmail.com>
2025-05-13 14:31:03 +0200

    J'ai ajouté une fonction de fou

--- R/functions.R
+++ R/functions.R
@@ -2,11 +2,11 @@ add_one <- function(x) {
   x + 1
 }
 
-# Example of Bob's changes for step 1a : uncomment lines 6 to 9
-# add_two <- function(x) {
-#   x <- add_one(x)
-#   return(add_one(x))
-# }
+add_two <- function(x) {
+  x <- add_one(add_one(x))
+  return(x)
+}
+
 
 # ** Uncomment and commit changes before starting scenario 2 **
 # add_n <- function(x, n) {
